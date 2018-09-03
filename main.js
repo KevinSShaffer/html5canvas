8ddb255493f3b2a023c80cde2354ed43cf5fa27d
kevinsshaffer <kevinsshaffer@gmail.com>
2018-09-02 20:27:30 -0600

    Circle Selection

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
 
+let circles = [];
+
 window.onload = () => {
     const canvas = document.getElementById('myCanvas');
     const context = canvas.getContext('2d');
 
-    canvas.onclick = e => {
-        const pos = getCursorPosition(canvas, e);
+    canvas.onclick = event => {
+        const pos = getCursorPosition(canvas, event);
+        const found = circles.find(circle => {
+            return isInsideCircle(pos.x, pos.y, circle);
+        });
 
-        context.beginPath();
-        context.arc(pos.x, pos.y, 5, 0, 2 * Math.PI);
-        context.fill();
+        if (found) {
+            context.fillStyle = 'green';
+            drawCircle(context, found);
+        } else {
+            const circle = { x: pos.x, y: pos.y, radius: 25 };
+            
+            context.fillStyle = 'black';
+            drawCircle(context, circle); 
+            circles.push(circle);
+        }
     };
-
-    const grades = [80, 90, 70, 100, 60, 80, 90];
-    const width = 45;
-    const startPos = { x: 50, y: 350 };
-
-    context.strokeStyle = 'white';
-    context.lineWidth = 2;
-
-    grades.forEach((grade, i) => {
-        let x = (i + 1) * startPos.x;
-
-        context.fillStyle = 'green';
-        context.fillRect(x, startPos.y, width, -grade);
-
-        context.fillStyle = 'red';
-        context.beginPath();
-        context.arc(x + (width / 2), startPos.y - grade, width / 2, 0, 1 * Math.PI, true);
-        context.fill();
-        //context.stroke();
-    });
 };
+
+function isInsideCircle(x, y, circle) {
+    x = Math.abs(x - circle.x);
+    if (x > circle.radius) return false;
+    y = Math.abs(y - circle.y);
+    if (y > circle.radius) return false;
+
+    return circle.radius >= Math.sqrt(x * x + y * y);
+}
+
+function drawCircle(context, circle) {
+    context.beginPath();
+    context.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    context.fill();
+}
 
 function getCursorPosition(canvas, event) {
     const rect = canvas.getBoundingClientRect();
